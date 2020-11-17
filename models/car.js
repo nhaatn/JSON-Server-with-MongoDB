344956b828b9ce1f20917eb037718a23c060981c
Nhat <nhaatn1@gmail.com>
2020-11-16 19:18:29 -0700

    tweaks to schema

--- models/car.js
+++ models/car.js
@@ -2,20 +2,13 @@ const mongoose = require('mongoose');
 
 // Make schema
 const carSchema = new mongoose.Schema(
+  
   {
-    id: {
-      type: Number, 
-      required: true
-    },
-    make: {
-      type: String, 
-      required: true
-    },
-    description: {
-      type: String, 
-      required: true
-    }
+    id:           Number,
+    title:        String,
+    description:  String,
   }
+  
 );
 
 module.exports = mongoose.model('car', carSchema);
